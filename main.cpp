49c720c9ae044930868b393bfc944a75f6ce61d9
danwardvs <danwardvs@adsgames.net>
2015-04-08 21:58:32 -0400

    Started working on "over the top"

--- main.cpp
+++ main.cpp
@@ -23,6 +23,11 @@ volatile int game_time = 0;
 int fps;
 int frames_done;
 int old_time;
+int timer;
+
+
+int reinforcements_german=1000;
+int reinforcements_canadian=1000;
 
 int is_battling;
 
@@ -70,6 +75,7 @@ void create_soldier(int new_x, int new_y, int new_type, int new_country){
     newSoldier.y=new_y;
     newSoldier.type=new_type;
     newSoldier.country=new_country;
+
     soldier.push_back(newSoldier);
 }
 
@@ -110,13 +116,42 @@ void abort_on_error(const char *message){
 }
 
 void update(){
-    if(random(1,100)==1){
-      is_battling=1;
+    if(timer>300){
+      //is_battling=1;
     }
-    for(int i=0; i<1000; i++){
-      if(soldier[i].type==3 && soldier[i].country==GERMAN && random(1,100)==1){
+    if(key[KEY_A]){
+      is_battling=15;//Over the top
+    }
+    timer++;
+    if(is_battling==1){
+      if(random(1,10)&& reinforcements_canadian>0){
+        create_soldier(random(0,1024),768,2,CANADIAN);
+      }
+    }
+
+    for(int i=0; i<soldier.size(); i++){
+      if(is_battling==15){
+        if(soldier[i].type==2 && soldier[i].country==CANADIAN){
+          if(soldier[i].y<580){
+            soldier[i].y--;
+          }
+          }
+      }
+      if(soldier[i].type==3 && soldier[i].country==GERMAN && random(1,100)==1 && is_battling==1){
         create_projectile(soldier[i].x,soldier[i].y,2);
       }
+      if(soldier[i].y>560 && soldier[i].country==CANADIAN && soldier[i].type==2){
+        soldier[i].y--;
+      }
+      if(soldier[i].type==2)
+        if(random(1,100)==1)soldier[i].x+=random(-5,5);
+      if(soldier[i].country==CANADIAN && soldier[i].y<500){
+        if(random(1,10)==1){
+          create_projectile(soldier[i].x,soldier[i].y,3);
+
+        }
+      }
+
     }
     for(int i=0; i<projectiles.size(); i++){
       if(projectiles[i].type==2){
@@ -130,14 +165,33 @@ void update(){
         for(int j=0; j<soldier.size(); j++){
               if(soldier[j].country==CANADIAN && soldier[j].type==2){
                  if(collision(projectiles[i].x-30,projectiles[i].x+30,soldier[j].x,soldier[j].x+10,projectiles[i].y-30,projectiles[i].y+30,soldier[j].y,soldier[j].y+10)){
-                  soldier.erase( soldier.begin() + j);
-              }
+                    if(soldier[j].country==GERMAN)reinforcements_german--;
+                    if(soldier[j].country==CANADIAN)reinforcements_canadian--;
+                    soldier.erase( soldier.begin() + j);
+
+                }
            }
 
         if(projectiles[i].type>25){
           projectiles.erase( projectiles.begin() + i);
 
         }
+
+      }
+
+    }else if(projectiles[i].type==3){
+      for(int j=0; j<soldier.size(); j++){
+          if(soldier[j].country==GERMAN && (soldier[j].type==2|| soldier[j].type==1)){
+                 if(collision(projectiles[i].x,projectiles[i].x+10,soldier[j].x,soldier[j].x+10,projectiles[i].y,projectiles[i].y+10,soldier[j].y,soldier[j].y+10) && random(1,5)==1){
+                    if(soldier[j].country==GERMAN)reinforcements_german--;
+                    soldier.erase( soldier.begin() + j);
+                    projectiles.erase( projectiles.begin() + i);
+
+                }
+           }
+
+
+
 
       }
 
@@ -165,9 +219,14 @@ void draw(){
     }
   for(int i=0; i<projectiles.size(); i++){
     if(projectiles[i].type==2)rectfill(buffer,projectiles[i].x,projectiles[i].y,projectiles[i].x+10,projectiles[i].y+10,makecol(0,255,0));
-    if(projectiles[i].type>9)rectfill(buffer,projectiles[i].x,projectiles[i].y,projectiles[i].x+50,projectiles[i].y+50,makecol(255,255,0));
+    if(projectiles[i].type>9 && projectiles[i].type<50 )rectfill(buffer,projectiles[i].x,projectiles[i].y,projectiles[i].x+50,projectiles[i].y+50,makecol(255,255,0));
 
   }
+  textprintf_ex(buffer,font,10,10,makecol(0,0,0),-1,"%i",reinforcements_german);
+  textprintf_ex(buffer,font,10,758,makecol(0,0,0),-1,"%i",reinforcements_canadian);
+  textprintf_ex(buffer,font,10,20,makecol(0,0,0),-1,"%i",timer/60);
+  textprintf_ex(buffer,font,10,30,makecol(0,0,0),-1,"%i",is_battling);
+
 
   draw_sprite(screen,buffer,0,0);
 }
